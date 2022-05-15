d3ba894f1adfbe5bdecf25cd4479e98874fb67f2
StShadow <StShadow@users.noreply.github.com>
2022-05-15 15:30:47 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="StShadow"
+original_name="location_history_converter"
+original_urlname="location-history-converter"
+original_description="Awesome location_history_converter created by StShadow"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv location_history_converter $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from location_history_converter.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "location_history_converter"
