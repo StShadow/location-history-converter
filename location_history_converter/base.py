2889836b113cf69dfb0486ade1e01994e346d39c
StShadow <st.shadow.by@gmail.com>
2022-05-27 22:06:40 +0200

    [#5] Add function to load custom or default files

--- location_history_converter/base.py
+++ location_history_converter/base.py
@@ -12,6 +12,44 @@ If you want to replace this with a Flask application run:
 
 and then choose `flask` as template.
 """
+import reverse_geocoder as rg
+import io
 
 # example constant variable
 NAME = "location_history_converter"
+
+"""
+So, long story short:
+
+1. load file with cities
+1.1 init reverse_geocoding module
+
+2. load location history file
+2.1 convert it to a list of coordinates
+4. reverse coordinates one-by-one
+4.1 reverse only first coordinate this day and skip the rest
+4.2 add to map day<->country
+5. dump map to a csv file
+"""
+
+
+def init_geocoder(geocode_filename=None):
+
+    if geocode_filename is None:
+        return rg
+    else:
+        geocoder = Object()
+        geocoder.name = "CustomerGeocoder"
+        geo = rg.RGeocoder(
+            mode=2,
+            verbose=True,
+            stream=io.StringIO(
+                open(geocode_filename, encoding="utf-8").read()
+            ),
+        )
+        geocoder.search = geo.query
+        return geocoder
+
+
+class Object(object):
+    pass
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,33 @@
-from location_history_converter.base import NAME
+import location_history_converter.base as base
+import tempfile
+from _pytest.tmpdir import tmp_path
 
 
 def test_base():
-    assert NAME == "location_history_converter"
+    assert base.NAME == "location_history_converter"
+
+
+def test_init_default_geocoder():
+    geocoder = base.init_geocoder()
+    assert geocoder.__name__ == "reverse_geocoder"
+
+
+def test_init_customer_geocoder(tmp_path):
+    tmp_file = tmp_path / "myfile"
+    tmp_file.touch()
+    tmp_file.write_text(
+        "lat,lon,name,admin1,admin2,cc\n42.57952,1.65362,El Tarter,Canillo,,AD",
+        encoding="utf-8",
+    )
+    print(tmp_file.name)
+    geocoder = base.init_geocoder(tmp_file)
+    assert geocoder.name == "CustomerGeocoder"
+
+
+#    with tempfile.NamedTemporaryFile() as tmp:
+#        print(tmp.name)
+#        tmp.write(b'this is some content')
+#        tmp.flush()
+#        tmp.seek(0)
+#        geocoder = base.init_geocoder(tmp.name)
+#        assert geocoder.__name__ == "Object"
